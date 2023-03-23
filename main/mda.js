5eca7969d45fb4b5b402c7a93cb019fe63b9433f
Alessandrotri <alezuz1988@gmail.com>
2023-03-23 14:43:03 +0100

    actualizacion de img

--- main/mda.js
+++ main/mda.js
@@ -3,7 +3,7 @@ const get = async () => {
   try {
     const res = await fetch("http://localhost:5000/museos");
     const resJson = await res.json();
-    console.log(resJson);
+    //console.log(resJson);
     return resJson;
   } catch (error) {
     console.log(error);
@@ -14,18 +14,18 @@ const drawMuseos = (museos) => {
   const divPadre$$ = document.querySelector(".divPadre");
   for (let i = 0; i < museos.length; i++) {
     const museo = museos[i];
-
+    console.log(museo);
     const divHijo$$ = document.createElement("div");
     divHijo$$.className = "divHijo";
     divHijo$$.innerHTML = `
         <div class="divPadre__hijo--arriba">
-        <img class="divPadre__hijo--arriba--img"/>
+        <img class="divPadre__hijo--arriba--img" src="${museo.image}" alt="${museo.name}">
         </div>
         <div class="divPadre__hijo--abajo">
-            <a href=""><h1 class="divPadre__hijo--abajo--nombre"></h1></a>
-            <h2 class="divPadre__hijo--abajo--localizacion"></h2>
-            <h3 class="divPadre__hijo--abajo--fecha"></h3>
-            <p class="divPadre__hijo--abajo--descripcion"></p>
+            <a href=""><h1 class="divPadre__hijo--abajo--nombre">${museo.name}</h1></a>
+            <h2 class="divPadre__hijo--abajo--localizacion">${museo.location}</h2>
+            <h3 class="divPadre__hijo--abajo--fecha">${museo.foundation}</h3>
+            <p class="divPadre__hijo--abajo--descripcion">${museo.description}</p>
         </div>
         `;
     divPadre$$.appendChild(divHijo$$);
